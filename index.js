cfae96497197ba18e5ae181261503ed6433cc50f
Hirak Roy <royhirakp@gmail.com>
2023-07-28 03:19:23 +0530

    create express app

---
+++ index.js
@@ -0,0 +1,43 @@
+// const server = require("./src/server");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ status: "working " });
+});
+app.get("/path", (req, res) => {
+  res.json({ status: "working /path" });
+});
+
+app.post("/", (req, res) => {
+  let data = req.body.data;
+
+  if (data === undefined) {
+    res.json({ mess: "plese provide data" });
+  } else {
+    res.json({ mess: data + "------data is comming----------" });
+  }
+});
+// server port
+const port = process.env.PORT || 5000;
+
+//CLUSTER
+const cluster = require("cluster");
+const os = require("os");
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  //connect db
+  //   require("./src/configDB/db");
+  // /server start
+  app.listen(port, () => {
+    console.log(`Server is up at ${port} `);
+  });
+}
