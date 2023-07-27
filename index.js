8416f9ce4eb88230835595054da37b481cfcee75
Hirak Roy <royhirakp@gmail.com>
2023-07-28 03:39:59 +0530

    update for aws test

--- index.js
+++ index.js
@@ -22,22 +22,25 @@ app.post("/", (req, res) => {
 // server port
 const port = process.env.PORT || 5000;
 
+app.listen(port, () => {
+  console.log(`Server is up at ${port} `);
+});
 //CLUSTER
-const cluster = require("cluster");
-const os = require("os");
+// const cluster = require("cluster");
+// const os = require("os");
 
-if (cluster.isPrimary) {
-  for (let i = 0; i < os.cpus().length; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  //connect db
-  //   require("./src/configDB/db");
-  // /server start
-  app.listen(port, () => {
-    console.log(`Server is up at ${port} `);
-  });
-}
+// if (cluster.isPrimary) {
+//   for (let i = 0; i < os.cpus().length; i++) {
+//     cluster.fork();
+//   }
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//   });
+// } else {
+//   //connect db
+//   //   require("./src/configDB/db");
+//   // /server start
+//   app.listen(port, () => {
+//     console.log(`Server is up at ${port} `);
+//   });
+// }
