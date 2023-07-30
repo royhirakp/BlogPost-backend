c44e60897d93b2b18d43593ea311e626abd4b94e
Hirak Roy <royhirakp@gmail.com>
2023-07-30 16:52:08 +0530

    cros add

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 // server port
 const port = process.env.PORT || 5000;
 const tokenVarifiction = require("./src/utils/jwtTokenVarification");
